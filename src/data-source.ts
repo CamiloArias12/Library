3d5183701e31c0991ce6f356062987619b203eac
CamiloArias12 <ariaspira13@gmail.com>
2023-05-07 17:10:40 -0500

    Add entities and resolvers

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User.js"
-
+import { entities } from "./entity/index.js"
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
     password: "123456",
-    database: "prueba",
+    database: "library",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities:entities, 
     migrations: [],
     subscribers: [],
 })
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { AppDataSource } from "./data-source.js";
-import { UserReolver } from "./resolvers/UserResolver.js";
-
+//import { UserResolver} from "./resolvers/UserResolver.js";
+import { AuthorResolver} from "./resolvers/AuthorResolvers.js";
+import {BookResolver} from './resolvers/BookResolvers.js'
+import { LoanResolver } from "./resolvers/LoanResolver.js";
+import { UserResolver } from "./resolvers/UserResolver.js";
 await AppDataSource.initialize()
 
 
 const schema = await  buildSchema ({
-   resolvers :[UserReolver],
+   resolvers :[UserResolver,LoanResolver],
    validate: {forbidUnknownValues: false}
 })
 
