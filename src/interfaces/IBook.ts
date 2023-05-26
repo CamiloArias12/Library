d25b21eec7cd2b939d84bd6295cd5baab08685c9
CamiloArias12 <ariaspira13@gmail.com>
2023-05-25 20:33:54 -0500

    Change values

--- src/interfaces/IBook.ts
+++ src/interfaces/IBook.ts
@@ -1,9 +1,7 @@
 export interface IBook {
-  id?: number;
-  title: string;
-  publisher: string;
-  area: string;
-  coverUrl?: string | null;
-  digitalUrl?: string | null;
-  physicalAvailable: boolean;
+  id: number
+  title: string
+  publisher: string
+  area: string
+  summary: string
 }
